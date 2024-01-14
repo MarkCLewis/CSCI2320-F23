eb0c27b30453c84e30459b55fb3a2e505600d749
Mark Lewis <mlewis@trinity.edu>
2024-01-14 09:49:24 -0600

    Adding to build for runs and tests.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,3 +41,20 @@ tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
+
+tasks.withType<JavaExec>() {
+    standardInput = System.`in`
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required = false
+        csv.required = true
+        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+    }
+}
